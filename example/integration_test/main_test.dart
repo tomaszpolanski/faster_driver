a8004138ff9672e690d570ff06939ab1725d6b5c
Tomek Polański <polanski.tomek@gmail.com>
2021-03-07 20:12:45 +0100

    Update main_test.dart

--- example/integration_test/main_test.dart
+++ example/integration_test/main_test.dart
@@ -2,9 +2,10 @@ import 'package:integration_test/integration_test.dart';
 
 //flutter drive --driver=test_driver/integration_test.dart --target=integration_test/main_test.dart -d windows --no-sound-null-safety
 import 'route_test.dart' as route;
+import 'simple_test.dart' as simple;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   route.main();
-  //simple.main();
+  simple.main();
 }
