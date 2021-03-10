5f0546e8ec4fb48b4ffda502cc459ef4d8b240b1
Tomek Polański <polanski.tomek@gmail.com>
2021-03-10 19:22:40 +0100

    disable screenshots on web

--- example/integration_test/screenshots.dart
+++ example/integration_test/screenshots.dart
@@ -1,20 +1,23 @@
 import 'dart:io';
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension WidgetTesterEx on WidgetTester {
   Future<void> takeScreenshot(String name) async {
-    RenderRepaintBoundary boundary =
-    firstRenderObject(find.byType(RepaintBoundary));
-    final image = await boundary.toImage();
-    final byteData = await image.toByteData(format: ImageByteFormat.png);
-    final pngBytes = byteData.buffer.asUint8List();
+    if (!kIsWeb) {
+      RenderRepaintBoundary boundary =
+          firstRenderObject(find.byType(RepaintBoundary));
+      final image = await boundary.toImage();
+      final byteData = await image.toByteData(format: ImageByteFormat.png);
+      final pngBytes = byteData.buffer.asUint8List();
 
-    final file = await File('${Directory.current.path}/screenshots/$name.png')
-        .create(recursive: true);
-    await file.writeAsBytes(pngBytes);
+      final file = await File('${Directory.current.path}/screenshots/$name.png')
+          .create(recursive: true);
+      await file.writeAsBytes(pngBytes);
+    }
   }
 }
