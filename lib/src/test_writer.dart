78cd5ea31450b63c8697ac52fd187f815b9b41f1
Tomek Polański <polanski.tomek@gmail.com>
2021-03-14 17:22:54 +0100

    Update test_writer.dart

--- lib/src/test_writer.dart
+++ lib/src/test_writer.dart
@@ -13,19 +13,28 @@ class TestWriter {
     );
     final sb = StringBuffer()
       ..writeln("import 'package:integration_test/integration_test.dart';")
-      ..writeln('');
+      ..writeln('')
+      ..writeln(_imports(files).join('\n'))
+      ..writeln('')
+      ..writeln('''
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();''')
+      ..writeln(_mains(files).join('\n'))
+      ..writeln('}');
+    _fileSystem.createFile(Uri.file(path), content: sb.toString());
+  }
+
+  Iterable<String> _imports(List<String> files) sync* {
     for (final file in files) {
       final name = p.basename(file);
-      sb.writeln("import '$name' as ${name.replaceAll('_test.dart', '')};");
+      yield "import '$name' as ${name.replaceAll('_test.dart', '')};";
     }
-    sb..writeln('')..writeln('''
-void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();''');
+  }
+
+  Iterable<String> _mains(List<String> files) sync* {
     for (final file in files) {
       final name = p.basename(file).replaceAll('_test.dart', '');
-      sb.writeln("  $name.main();");
+      yield "  $name.main();";
     }
-    sb.writeln('}');
-    _fileSystem.createFile(Uri.file(path), content: sb.toString());
   }
 }
