55620d0eed8992763b2dfda3a2a020ddf21c5d15
Tomek Polański <polanski.tomek@gmail.com>
2021-03-15 08:58:36 +0100

    wip

--- example/integration_test/main_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:integration_test/integration_test.dart';
-
-import 'simple_test.dart' as simple;
-
-void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  simple.main();
-}
