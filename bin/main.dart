3f6c1bf6373c487df3cae61b13c51b04a2b61451
Tomek Polański <polanski.tomek@gmail.com>
2021-03-22 12:30:57 +0100

    Improving successful message

--- bin/main.dart
+++ bin/main.dart
@@ -2,8 +2,16 @@ library faster_driver;
 
 import 'package:cli_util/cli_logging.dart';
 import 'package:faster_driver/faster_driver.dart';
+import 'package:faster_driver/src/file_system.dart';
+import 'package:faster_driver/src/test_writer.dart';
 
 Future<void> main(List<String> args) async {
   final logger = Logger.standard();
-  await run(args, logger: logger);
+  final fs = FileSystem();
+  final testWriter = TestWriter(fs);
+  await run(
+    args,
+    testWriter: testWriter,
+    logger: logger,
+  );
 }
--- lib/faster_driver.dart
+++ lib/faster_driver.dart
@@ -4,10 +4,12 @@ import 'package:cli_util/cli_logging.dart';
 import 'package:faster_driver/src/argument_parser.dart';
 import 'package:faster_driver/src/file_system.dart';
 import 'package:faster_driver/src/test_writer.dart';
+import 'package:faster_driver/src/utils/colorize/colorizing.dart';
 
 Future<void> run(
   List<String> args, {
   required Logger logger,
+  required TestWriter testWriter,
 }) async {
   try {
     final arguments = ArgumentParser().parse(args);
@@ -15,12 +17,21 @@ Future<void> run(
       logger.stdout(arguments.message);
       return;
     } else if (arguments is MainArgs) {
-      final fs = FileSystem();
-      final testWriter = TestWriter(fs);
-      await testWriter.generateMainTest(
+      final testFileCount = await testWriter.generateMainTest(
         directory: arguments.directory,
         fileName: arguments.file,
       );
+      if (testFileCount != 0) {
+        logger.stdout(
+          'Merged ${bold('$testFileCount')} test file(s) into '
+          'one ${bold(arguments.file)}',
+        );
+      } else {
+        logger.stdout(
+          '${red('No test')} were found. '
+          'Make sure the tests have postfix ${bold('_test.dart')}.',
+        );
+      }
     }
   } on ArgumentException catch (e) {
     logger.stderr(e.message);
